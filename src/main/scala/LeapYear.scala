0e168d8f96618650a6d60dfca3f82f68b2a23ede
cheluvesha <cheluvesh.b@gmail.com>
2020-09-28 17:57:54 +0530

    [CHELUVESHA]Refactored : comments and exceptions

--- src/main/scala/LeapYear.scala
+++ src/main/scala/LeapYear.scala
@@ -1,16 +1,26 @@
 object LeapYear {
-  def checkLeapYearOrNot(year: Int): Boolean = {
+  // condition which checks leap year
+  private def checkLeapYearOrNot(year: Int): Boolean = {
     (((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)))
   }
+  // main method
   def main(args: Array[String]): Unit = {
+    try {
     println("Please Enter a Four Digit based Year to check: ")
-    val year = scala.io.StdIn.readInt()
-    val conclusion = checkLeapYearOrNot(year)
-    if (conclusion) {
-      println(s"$year is a leap year")
+    val Year = scala.io.StdIn.readInt()
+    val Conclusion = checkLeapYearOrNot(Year)
+    if (Conclusion) {
+      println(s"$Year is a leap Year")
     }
     else {
-      println(s"$year is not a leap year")
+      println(s"$Year is not a leap Year")
+    }
+  }
+    catch {
+      case numberFormatException: NumberFormatException =>
+        println("Please Enter a Proper Four Digit NUmber")
+      case exception: Exception =>
+        println(exception.printStackTrace())
     }
   }
 }
