e12c234058ba8325144faac3e0263e4455f08efb
cheluvesha <cheluvesh.b@gmail.com>
2020-09-28 17:58:16 +0530

    [CHELUVESHA]Refactored : comments and exceptions

--- src/main/scala/PrimeFactors.scala
+++ src/main/scala/PrimeFactors.scala
@@ -1,5 +1,6 @@
 object PrimeFactors {
-  def factorsGenerate(number: Int): Unit = {
+  // method which generates factors
+  private def factorsGenerate(number: Int): Unit = {
     var value = number
     for (digit <- 2 to value) {
       while (value % digit == 0) {
@@ -8,9 +9,18 @@ object PrimeFactors {
       }
     }
   }
+  // main method
   def main(args: Array[String]): Unit = {
+    try {
     println("Enter the Number to find prime factors: ")
     var number = scala.io.StdIn.readInt()
     factorsGenerate(number)
+  }
+    catch {
+      case numberFormatException: NumberFormatException =>
+        println("Please Enter a Proper Number")
+      case exception: Exception =>
+        println(exception.printStackTrace())
+    }
   }
 }
