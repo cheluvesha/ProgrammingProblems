1957883eff3f6ad90122b40645b3f24284cf6ba8
cheluvesha <cheluvesh.b@gmail.com>
2020-09-28 17:55:46 +0530

    [CHELUVESHA]Refactored : comments and exceptions

--- src/main/scala/FlipCoin.scala
+++ src/main/scala/FlipCoin.scala
@@ -1,26 +1,39 @@
+import java.lang.NumberFormatException
+
 import scala.util.Random
 
 object FlipCoin {
-  val VALUE = 100
-  def coinPercentage(): Unit = {
+  // to calculate percentage
+  val Value = 100
+  // random number generate and calculating the percentage
+  private def coinPercentage(): Unit = {
     var headCount = 0
     var tailCount = 0
     println("Please Enter the Number of times to FlipCoin : ")
-    var tossNumber = scala.io.StdIn.readInt()
-    while (tossNumber > 0) {
-      var result = Random.nextInt(2)
-      if (result == 1) {
-        headCount += 1
+    try {
+      var tossNumber = scala.io.StdIn.readInt()
+      while (tossNumber > 0) {
+        val Result = Random.nextInt(2)
+        if (Result == 1) {
+          headCount += 1
+        }
+        else {
+          tailCount += 1
+        }
+        tossNumber -= 1
       }
-      else {
-        tailCount += 1
-      }
-      tossNumber -= 1
+      val HeadPercentage = (headCount.toFloat / (headCount + tailCount)) * Value
+      val TailPercentage = Value - HeadPercentage
+      println(f"Head Percentage : $HeadPercentage%.0f%% and Tail Percentage $TailPercentage%.0f%%")
     }
-    val headPercentage = (headCount.toFloat / (headCount+ tailCount)) * VALUE
-    val tailPercentage = VALUE - headPercentage
-    println(f"Head Percentage : $headPercentage%.0f%% and Tail Percentage $tailPercentage%.0f%%")
+    catch {
+      case numberFormatException: NumberFormatException =>
+        println("Please Enter a Proper Number")
+      case exception: Exception =>
+        println(exception.printStackTrace())
+    }
   }
+  // main method which calls coinPercentage method
   def main(args: Array[String]): Unit = {
     coinPercentage()
   }
