75cc1aa671c07a85d113662c562f60a1bb0fb1f4
cheluvesha <cheluvesh.b@gmail.com>
2020-09-29 07:15:26 +0530

    [CHELUVESHA]Added : calculates time lapse between start and end function

---
+++ src/main/scala/StopWatch.scala
@@ -0,0 +1,27 @@
+import java.util.concurrent.TimeUnit
+import scala.io.StdIn.readLine
+
+object StopWatch {
+
+  // calculates time lapse between start and end
+  def stopWatchCall(): Unit = {
+    try {
+      var start: Long = 0
+      var end: Long = 0
+      var press: String = readLine("Enter S key to start stopwatch:")
+      start = System.nanoTime()
+      press = readLine("Enter R key to stop stopwatch:")
+      end = System.nanoTime()
+      println("%d Secs".format(TimeUnit.SECONDS.convert(end - start, TimeUnit.NANOSECONDS)))
+    }
+    catch {
+      case exception: Exception =>
+        println(exception.printStackTrace())
+    }
+  }
+
+  // main method
+  def main(args: Array[String]): Unit = {
+    stopWatchCall()
+  }
+}
